009947a95b1b0ee8f22a9da2c9fcfe2220e2eb6f
YL_HomeY <13821082522@qq.com>
2018-04-05 14:34:37 +0800

    home

---
+++ class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018-04-05 14:31
+# @Author  : Little.Yang
+# @Site    : 
+# @File    : class.py
+# @Software: PyCharm
+
+l=[1,2,2,2,4]
+d={(23,33):33}
+s=set(l)
+print (s)
